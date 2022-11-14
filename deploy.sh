1feb2615f470d0afc6e47a3a3058351b17dc016f
metroz1 <wlstjr132@gmail.com>
2022-11-14 14:51:14 +0900

    Modification deploy.sh JAR_REPOSITORY

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/app/build/libs
-JAR_REPOSITORY=/home/ubuntu/app/
+JAR_REPOSITORY=/home/ubuntu/app/build/libs
+REPOSITORY=/home/ubuntu/app/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
