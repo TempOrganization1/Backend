8ec996c3b233da31648cc06a10b2588b7f932afb
metroz1 <wlstjr132@gmail.com>
2022-11-29 17:40:22 +0900

    Delete interceptor in WebSocketConfig

--- src/main/java/com/sparta/actualpractice/chat/MessageController.java
+++ src/main/java/com/sparta/actualpractice/chat/MessageController.java
@@ -1,9 +1,13 @@
 package com.sparta.actualpractice.chat;
 
+import com.sparta.actualpractice.security.MemberDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,7 +33,7 @@ public class MessageController {
     public void addMessage(MessageRequestDto messageRequestDto,
                            @PathVariable("chatRoom_id") Long chatRoomId, @Header("Authorization") String token) {
 
-        System.out.println("token = " + token);
+
         messageService.sendMessage(messageRequestDto, chatRoomId, token);
     }
 
