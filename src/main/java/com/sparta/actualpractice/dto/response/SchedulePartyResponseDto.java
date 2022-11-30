c240197c296abfd7957591a3914f78b81fbea89a
metroz1 <wlstjr132@gmail.com>
2022-12-01 01:48:12 +0900

    Add chatRoomId in SchedulePartyResponseDto

--- src/main/java/com/sparta/actualpractice/dto/response/SchedulePartyResponseDto.java
+++ src/main/java/com/sparta/actualpractice/dto/response/SchedulePartyResponseDto.java
@@ -22,7 +22,8 @@ public class SchedulePartyResponseDto {
     private String partyIntroduction;
     private Boolean isParticipant;
     private int participantSize;
-    private List<MemberResponseDto> memberResponseDtoList;
+    private List<MemberResponseDto> participantList;
+    private Long chatRoomId;
 
     public SchedulePartyResponseDto(Schedule schedule, Boolean isParticipant, Party party, List<MemberResponseDto> memberResponseDtoList) {
 
@@ -39,6 +40,7 @@ public class SchedulePartyResponseDto {
         this.isParticipant = isParticipant;
         this.partyName = party.getName();
         this.partyIntroduction = party.getIntroduction();
-        this.memberResponseDtoList = memberResponseDtoList;
+        this.participantList = memberResponseDtoList;
+        this.chatRoomId = party.getChatRoom().getId();
     }
 }
