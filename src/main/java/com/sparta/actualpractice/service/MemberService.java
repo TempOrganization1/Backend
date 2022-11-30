7e7ebff368da720cae3aec57f046449ca34bbd00
metroz1 <wlstjr132@gmail.com>
2022-12-01 02:02:11 +0900

    Add ChatRoom Creation in first party

--- src/main/java/com/sparta/actualpractice/service/MemberService.java
+++ src/main/java/com/sparta/actualpractice/service/MemberService.java
@@ -4,14 +4,8 @@ import com.sparta.actualpractice.dto.TokenDto;
 import com.sparta.actualpractice.dto.request.MemberInfoRequestDto;
 import com.sparta.actualpractice.dto.request.MemberRequestDto;
 import com.sparta.actualpractice.dto.response.MemberResponseDto;
-import com.sparta.actualpractice.entity.Member;
-import com.sparta.actualpractice.entity.MemberParty;
-import com.sparta.actualpractice.entity.Party;
-import com.sparta.actualpractice.entity.RefreshToken;
-import com.sparta.actualpractice.repository.MemberPartyRepository;
-import com.sparta.actualpractice.repository.MemberRepository;
-import com.sparta.actualpractice.repository.PartyRepository;
-import com.sparta.actualpractice.repository.RefreshTokenRepository;
+import com.sparta.actualpractice.entity.*;
+import com.sparta.actualpractice.repository.*;
 import com.sparta.actualpractice.security.JwtFilter;
 import com.sparta.actualpractice.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -42,6 +36,7 @@ public class MemberService {
     private final RefreshTokenRepository refreshTokenRepository;
     private final PartyRepository partyRepository;
     private final MemberPartyRepository memberPartyRepository;
+    private final ChatRoomRepository chatRoomRepository;
 
     public ResponseEntity<?> signup(MemberRequestDto memberRequestDto) {
 
@@ -61,7 +56,9 @@ public class MemberService {
                     "\uD83D\uDE46\uD83C\uDFFB\u200D♀️ 새로운 그룹을 만들면 초대 코드를 통해 친구들과 소중한 추억을 공유하실 수 있습니다 !";
 
             Party party = new Party(name, introduction);
+            ChatRoom chatRoom = new ChatRoom(party, party.getName() + "의 채팅방");
 
+            chatRoomRepository.save(chatRoom);
             partyRepository.save(party);
         }
 
