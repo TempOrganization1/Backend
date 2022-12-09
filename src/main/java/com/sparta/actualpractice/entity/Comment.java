d92be550b5254b8f838a9c0c8839badaad9af831
kimjiae970 <kimjiae970@gmail.com>
2022-12-09 14:38:11 +0900

    FIX modify maximum Comment length

--- src/main/java/com/sparta/actualpractice/entity/Comment.java
+++ src/main/java/com/sparta/actualpractice/entity/Comment.java
@@ -17,7 +17,7 @@ public class Comment extends TimeStamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 300)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
