0ffdfab8e485b2387e5e759abf13fa8ccb9d0c2a
kimjiae970 <kimjiae970@gmail.com>
2022-12-09 14:25:21 +0900

    FIX modify mypage image null error

--- src/main/java/com/sparta/actualpractice/service/MemberService.java
+++ src/main/java/com/sparta/actualpractice/service/MemberService.java
@@ -91,9 +91,14 @@ public class MemberService {
 
         Member member1 = memberRepository.findByEmail(member.getEmail()).orElseThrow(() -> new NullPointerException("해당 사용자를 찾을 수 없습니다."));
 
-        String imageUrl = s3UploadService.upload(memberInfoRequestDto.getProfileImageUrl(), dir);
+        if (!memberInfoRequestDto.getProfileImageUrl().isEmpty()) {
 
-        member1.updateInfomation(imageUrl, memberInfoRequestDto.getMemberName());
+            String imageUrl = s3UploadService.upload(memberInfoRequestDto.getProfileImageUrl(), dir);
+
+            member1.updateImage(imageUrl);
+        }
+
+        member1.updateName(memberInfoRequestDto.getMemberName());
 
         return new ResponseEntity<>(new MemberResponseDto(member1), HttpStatus.OK);
     }
