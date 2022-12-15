bef392f98975f796b1877d3b8452d73c90f45143
metroz1 <wlstjr132@gmail.com>
2022-12-15 15:51:14 +0900

    Add Simple Test Code

---
+++ src/test/java/com/sparta/actualpractice/member/MemberControllerTest.java
@@ -0,0 +1,44 @@
+package com.sparta.actualpractice.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Rollback;
+
+@SpringBootTest
+class MemberControllerTest {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public MemberControllerTest(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Test
+    @Rollback
+    @DisplayName("회원가입 테스트")
+    void signup() {
+
+        Member member = Member.builder()
+                .email("1234@gmail.com")
+                .name("테스트")
+                .password(passwordEncoder.encode("asdf1234"))
+                .build();
+
+        Assertions.assertThat(member.getEmail()).isEqualTo("1234@gmail.com");
+        Assertions.assertThat(member.getName()).isEqualTo("다르게");
+
+        memberRepository.save(member);
+    }
+
+    @Test
+    void checkEmail() {
+    }
+}
